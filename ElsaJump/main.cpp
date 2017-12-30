b6aed06e7165ae5725ac0d21d1a6e04fdf5755d7
srimshady <monzeiger@gmail.com>
2017-12-30 12:47:55 -0800

    Initial Commit

---
+++ ElsaJump/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ElsaJump
+//
+//  Created by Simón Zeiger on 12/30/17.
+//  Copyright © 2017 Simón Zeiger. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
